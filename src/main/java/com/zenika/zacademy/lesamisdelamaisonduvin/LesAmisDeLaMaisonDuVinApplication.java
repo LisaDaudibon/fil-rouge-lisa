e7a33ddccff1f872a3753a51e94074321f20e06b
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-02-07 09:40:58 +0100

    :tada: initial commit

---
+++ src/main/java/com/zenika/zacademy/lesamisdelamaisonduvin/LesAmisDeLaMaisonDuVinApplication.java
@@ -0,0 +1,13 @@
+package com.zenika.zacademy.lesamisdelamaisonduvin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesAmisDeLaMaisonDuVinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesAmisDeLaMaisonDuVinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zenika/zacademy/lesamisdelamaisonduvin/LesAmisDeLaMaisonDuVinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenika.zacademy.lesamisdelamaisonduvin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LesAmisDeLaMaisonDuVinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
