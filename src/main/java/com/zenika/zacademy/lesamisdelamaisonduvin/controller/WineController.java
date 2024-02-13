bd1a02274876d63ea846deeae63ef8087afb336b
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-02-13 21:19:27 +0100

    :recycle: changing types Wine in WineDtoIn or Out

--- src/main/java/com/zenika/zacademy/lesamisdelamaisonduvin/controller/WineController.java
+++ src/main/java/com/zenika/zacademy/lesamisdelamaisonduvin/controller/WineController.java
@@ -1,47 +1,60 @@
 package com.zenika.zacademy.lesamisdelamaisonduvin.controller;
 
+import com.zenika.zacademy.lesamisdelamaisonduvin.controller.dto.WineDtoIn;
+import com.zenika.zacademy.lesamisdelamaisonduvin.controller.dto.WineDtoOut;
+import com.zenika.zacademy.lesamisdelamaisonduvin.controller.dto.WineMapper;
 import com.zenika.zacademy.lesamisdelamaisonduvin.service.WineService;
+import com.zenika.zacademy.lesamisdelamaisonduvin.service.exception.BadRequestException;
 import com.zenika.zacademy.lesamisdelamaisonduvin.service.model.Wine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/wines")
 public class WineController {
     private final Logger logger = LoggerFactory.getLogger(WineController.class);
+
     private WineService wineService;
+    private WineMapper wineMapper;
+
     //TODO - Ajouter le mappeur WineDtoIn
-    public WineController ( WineService wineService) {
+    public WineController ( WineService wineService, WineMapper wineMapper) {
         this.wineService = wineService;
+        this.wineMapper = wineMapper;
     }
 
     //Port 8082
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Wine> getAll () {
+    public List<WineDtoOut> getAll () {
         logger.info("Return a list of all wines");
-        return wineService.getAll();
+        List<Wine> wines = wineService.getAll();
+        List <WineDtoOut> winesDtoOut = new ArrayList<>();
+        for (Wine wine : wines)
+            winesDtoOut.add(wineMapper.toDto(wine));
+        return winesDtoOut;
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Wine getOneById (@PathVariable("id") int searchedId) {
+    public WineDtoOut getOneById (@PathVariable("id") int searchedId) {
         // TODO - Gérer les cas d'erreur (try/catch + exception)
         logger.info("Return the wine associated with the id " + searchedId);
-        return wineService.getOneById(searchedId);
+        return wineMapper.toDto(wineService.getOneById(searchedId));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Wine create (@RequestBody Wine newWine) {
+    public WineDtoOut create (@RequestBody WineDtoIn newWine) throws BadRequestException {
         //TODO - Utiliser Hibernate Validator
-        Wine wineAdded = wineService.create(newWine);
+        Wine wineAdded = this.wineService.create(wineMapper.toModel(newWine));
         logger.info("Wine created : " + wineAdded);
-        return wineAdded;
+        return this.wineMapper.toDto(wineAdded);
     }
 
     @DeleteMapping("/{id}")
@@ -54,9 +67,9 @@ public class WineController {
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Wine updateById (@PathVariable("id") int searchedId, @RequestBody Wine wineUpdated) {
+    public WineDtoOut updateById (@PathVariable("id") int searchedId, @RequestBody WineDtoIn wineUpdated) throws BadRequestException {
         // TODO - Gérer les cas d'erreur (try / catch + exception)
         logger.info("Update a Wine with id " + searchedId);
-        return wineService.updateById(searchedId, wineUpdated);
+        return wineMapper.toDto(wineService.updateById(searchedId, wineMapper.toModel(wineUpdated)));
     }
 }
